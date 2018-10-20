9ad27e588544e884f679c4bfb377c07abc791090
Sanat Sharma <sanatcolumban@gmail.com>
2018-10-20 18:41:27 -0500

    Update input dimensions

--- model/search.py
+++ model/search.py
@@ -6,4 +6,27 @@ import argparse
 import sys
 from model import *
 from utils import *
+from search_data import *
 
+# Read in command line arguments to the system
+def arg_parse():
+    parser = argparse.ArgumentParser(description='trainer.py')
+    parser.add_argument('--model', type=str, default='Neural', help="Model to run")
+    parser.add_argument('--train_type', type=str, default="CIFAR10", help="Data type - Cifar10 or custom")
+    parser.add_argument('--train_path', type=str, default='../data/Reference/', help='Path to the training set')
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = arg_parse()
+    print(args)
+
+    if (args.model == 'Neural'):
+        # Get images from train path and call CNN model train function
+        if (args.train_type == 'CIFAR10'):
+            train_data, test_data = get_cifar_data()
+            train_neural_model(train_data, test_data)
+
+    else:
+        raise Exception("Please select appropriate model")
+    
--- model/search_data.py
+++ model/search_data.py
@@ -7,14 +7,15 @@ from torchvision import transforms, datasets
 
 def get_cifar_data():
     transform = transforms.Compose([
+            transforms.CenterCrop((224,224)),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
 
     trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=2, shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=2, shuffle=False, num_workers=2)
 
     return trainloader, testloader
