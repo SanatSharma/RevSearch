5c842fe69f0e20e0d97f69b7d9e32ee6bb5b1c06
Sanat Sharma <sanatcolumban@gmail.com>
2018-10-20 21:05:59 -0500

    Model change

--- model/model.py
+++ model/model.py
@@ -18,8 +18,8 @@ class Neural(nn.Module):
     def __init__(self):
         super(Neural, self).__init__()
         # Use pretrained resnet model
-        self.resnet = models.resnet18(pretrained=True)
-        self.new_resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
+        resnet = models.vgg11(pretrained=True)
+        self.new_resnet = torch.nn.Sequential(*(list(resnet.children())[:-1]))
         # Decompose new_resnet features into a vector of size 300
         self.pca = PCA(n_components=300)
         print(self.new_resnet)
@@ -36,24 +36,25 @@ class Neural(nn.Module):
 def train_neural_model(train_data, test_data):
     model = Neural()
     model.to(device)
+    model.eval()
     epochs = 1
     neural_net_output_size = 512
-
-    for epoch in range(epochs):
-        print(str(epoch) + " of " + str(epochs) + " epochs")
-        neural_feats = torch.zeros([len(train_data), neural_net_output_size])
-        for batch_idx, (inputs, outputs) in enumerate(train_data):
-            if (batch_idx%100==0):
-                print(str(batch_idx) + " of " + str(len(train_data)) + " examples")
-            inputs = inputs.to(device)
-            feats = model.forward(inputs)
-            for i in range(len(feats)):
-                neural_feats[batch_idx*2 + i] = feats[i,:,0,0]
-        
-        print(neural_feats)
-        file = open('dump.txt', 'w')
-        pickle.dump(neural_feats, file)
-        file.close()
+    with torch.no_grad():
+        for epoch in range(epochs):
+            print(str(epoch) + " of " + str(epochs) + " epochs")
+            neural_feats = torch.zeros([len(train_data), neural_net_output_size])
+            for batch_idx, (inputs, outputs) in enumerate(train_data):
+                if (batch_idx%100==0):
+                    print(str(batch_idx) + " of " + str(len(train_data)) + " examples")
+                inputs = inputs.to(device)
+                feats = model.forward(inputs)
+                for i in range(len(feats)):
+                    neural_feats[batch_idx*2 + i] = feats[i,:,0,0]
+            
+            print(neural_feats)
+            file = open('dump.txt', 'w')
+            pickle.dump(neural_feats, file)
+            file.close()
 
 
     raise Exception("Implement training")
--- model/search_data.py
+++ model/search_data.py
@@ -13,7 +13,7 @@ def get_cifar_data():
         ])
 
     trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=2, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=10, shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=2, shuffle=False, num_workers=2)
