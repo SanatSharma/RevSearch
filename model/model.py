496fee066a34dde749bd449fc1eb4543703bede2
Sanat Sharma <sanatcolumban@gmail.com>
2018-10-21 03:06:52 +0000

    Initial training features

--- model/model.py
+++ model/model.py
@@ -23,11 +23,11 @@ class Neural(nn.Module):
         # Decompose new_resnet features into a vector of size 300
         self.pca = PCA(n_components=300)
         print(self.new_resnet)
-    
+
     def forward(self, x, batch_size=1):
         feats = self.new_resnet(x)
         return feats
-    
+
     def reduce_dimensionality(self, feats):
         self.pca.fit(features)
         reduced_feats = self.pca.transform(features)
@@ -38,23 +38,23 @@ def train_neural_model(train_data, test_data):
     model.to(device)
     model.eval()
     epochs = 1
+    batch_size = 10
     neural_net_output_size = 512
     with torch.no_grad():
         for epoch in range(epochs):
             print(str(epoch) + " of " + str(epochs) + " epochs")
-            neural_feats = torch.zeros([len(train_data), neural_net_output_size])
+            neural_feats = torch.zeros([len(train_data)*batch_size, neural_net_output_size])
             for batch_idx, (inputs, outputs) in enumerate(train_data):
                 if (batch_idx%100==0):
                     print(str(batch_idx) + " of " + str(len(train_data)) + " examples")
                 inputs = inputs.to(device)
                 feats = model.forward(inputs)
                 for i in range(len(feats)):
-                    neural_feats[batch_idx*2 + i] = feats[i,:,0,0]
-            
+                    neural_feats[batch_idx*batch_size + i] = feats[i,:,0,0]
+
             print(neural_feats)
-            file = open('dump.txt', 'w')
-            pickle.dump(neural_feats, file)
-            file.close()
+            torch.save(neural_feats, 'torch.pt')
+
 
 
     raise Exception("Implement training")
