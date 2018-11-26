b713a6e98553fcd695f005e2f4af29fc7ac18b3e
Sanat Sharma <sanatcolumban@gmail.com>
2018-11-26 13:51:54 -0600

    Small change:

--- model/inverted_file.py
+++ model/inverted_file.py
@@ -1,3 +1,5 @@
+from utils import *
+from scipy.cluster.vq import kmeans2
 # Loop over all training images
 # Read in sift features for training images
 # construct textons using kmeans 
@@ -11,9 +13,20 @@
 # The image with the maximum amount of words should rank first.
 # For images with same amount of words, select randomly?
 
-def train_ml_model(train_data, image_indexer):
-    for batch_idx, (input, output) in enumerate(train_data):
-        print("INPUT")
-        print(input)
-        print("OUTPUT")
-        print(output)
+class MLModel:
+    def __init__(self, word_indexer, image_indexer):
+        self.word_indexer = word_indexer
+        self.image_indexer = image_indexer
+    def evaluate (self, test_data):
+        pass
+
+def train_ml_model(train_data, image_indexer, sift_features, args):
+    image_words, _ = kmeans2(sift_features[0], args.num_clusters)
+
+    # Construct index -> word indexer
+    word_indexer = Indexer()
+    for i in image_words:
+        word_indexer.get_index(i)
+
+    # Construct inverted file index of word_index to image_index list
+    
